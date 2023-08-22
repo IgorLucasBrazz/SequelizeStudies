1bfd8f3dfb783b804d4ceaeeb3206fe60641c5a0
IgorLucasBrazz <igor.braz@ufv.br>
2023-08-22 09:31:16 -0300

    feat: Rota com um id especifico adicionada.

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -1,16 +1,34 @@
 const database = require('../models');
 
 class PessoaController {
-    static async pegaTodasAsPessoas(req, res){
-        try{
-        
+    static async pegaTodasAsPessoas(req, res) {
+        try {
+
             const todasAsPessoas = await database.Pessoas.findAll()
             return res.status(200).json(todasAsPessoas)
-        }catch (error){
+        } catch (error) {
             return res.status(500).json(error.message)
         }
-        
+
+    }
+
+    static async pegaUmaPessoa(req, res) {
+        const { id } = req.params
+        try {
+            const umaPessoa = await database.Pessoas.findOne(
+                {
+                    where:
+                    {
+                        id: Number(id)
+                    }
+                })
+            return res.status(200).json(umaPessoa)
+        } catch (error) {
+            return res.status(500).json(error.message)
+        }
     }
 }
+
+
 
 module.exports = PessoaController
--- api/routes/pessoasRoutes.js
+++ api/routes/pessoasRoutes.js
@@ -5,4 +5,6 @@ const router = Router()
 
 router.get('/pessoas', PessoaController.pegaTodasAsPessoas)
 
+router.get('/pessoas/:id', PessoaController.pegaUmaPessoa)
+
 module.exports = router
