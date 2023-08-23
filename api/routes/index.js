62ebfed1aafa3166fb0cdb2e97eb2f6f8ab5c19c
IgorLucasBrazz <igor.braz@ufv.br>
2023-08-23 18:50:32 -0300

    fix: Bugs resolvidos!

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,7 +1,19 @@
-const bodyParser = require('body-parser')
-const pessoas = require('./pessoasRoute')
+//routes/index.js
 
+const bodyParser = require('body-parser')
+
+const pessoas = require('./pessoasRoute')
+//adicionamos as rotas de niveis e turmas
+const niveis = require('./niveisRoute')
+const turmas = require('./turmasRoute')
+
+//adicionamos as instâncias de níveis e turmas
+//e refatoramos um pouco a função
 module.exports = app => {
-  app.use(bodyParser.json())
-  app.use(pessoas)
-}
+ app.use(
+   bodyParser.json(),
+   pessoas,
+   niveis,
+   turmas
+   )
+ }
