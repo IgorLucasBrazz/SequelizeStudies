6e0efa52ef582d2ea9c20239d867949eefc81e84
IgorLucasBrazz <igor.braz@ufv.br>
2023-08-23 11:00:59 -0300

    feat: adicionado novos controllers e routers.

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,8 +1,14 @@
 const bodyParser = require('body-parser')
 const pessoas = require('./pessoasRoutes')
+const niveis = require('./niveisRoute')
+const turmas = require('./turmasRoute')
 
 module.exports = app => {
     app.use(bodyParser.json())
-    app.use(pessoas)
+    app.use(
+        pessoas,
+        niveis,
+        turmas        
+        )
     //app.get('/', (req, res) => res.send('ola'))
 }
